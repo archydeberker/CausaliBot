817f69d1a1d3f4046b27199efdecc93894ac9864
Peter <peter@hingehealth.comgit config --global user.email peter@hingehealth.comgit config --global user.email peter@hingehealth.com>
2017-01-29 21:02:56 +0000

    fricking indentation

--- database/wit.py
+++ database/wit.py
@@ -4,7 +4,7 @@ def understand_string(message_text):
 	# send a string to wit and get structured info back.
 	# For list of headers see https://www.cs.tut.fi/~jkorpela/http.html
 	# Returns a dictionary if the call was successful
-    rq = urllib2.Request('https://api.wit.ai/message?v=20170129&q='+message_text)
+	rq = urllib2.Request('https://api.wit.ai/message?v=20170129&q='+message_text)
 	rq.add_header("Authorization", "Bearer FS4CJQVZGWFNJ525V5JJ7NVR5SWBDUIG")
 	rq.add_header('Content-Type', 'text/plain')
 
