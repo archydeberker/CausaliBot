893e1c42ce934e3a58afbb3a7a02059a87403365
Peter <peter@hingehealth.comgit config --global user.email peter@hingehealth.comgit config --global user.email peter@hingehealth.com>
2017-01-29 21:45:44 +0000

    better wit API calls and parse times

--- database/wit.py
+++ database/wit.py
@@ -1,13 +1,18 @@
 # set of functions to interact with the Wit AI
 
 import json
+import urllib
 import urllib2
+import datetime
 
 def understand_string(message_text):
 	# send a string to wit and get structured info back.
 	# For list of headers see https://www.cs.tut.fi/~jkorpela/http.html
-	# Returns a dictionary if the call was successful
-	rq = urllib2.Request('https://api.wit.ai/message?v=20170129&q='+message_text)
+	# Returns a dictionary if the call was successful.
+
+	# make sure the message_text is allowed to have spaces
+	query = {'q': message_text}
+	rq = urllib2.Request('https://api.wit.ai/message?q='+urllib.urlencode(query))
 	rq.add_header("Authorization", "Bearer FS4CJQVZGWFNJ525V5JJ7NVR5SWBDUIG")
 	rq.add_header('Content-Type', 'text/plain')
 
@@ -19,3 +24,12 @@ def understand_string(message_text):
 		print('Request to Wit failed for message:', message_text)
 		print('Status code:', resp.getcode())  # print the code
 		return {}
+
+def timestamp_to_simple_string(wit_object):
+	""" Takes a with object that has detected a dateimeand makes it into a simple string that Causali code can understand
+	
+	Example input: "2017-01-30T07:00:00.000-08:00"
+	"""
+	t = wit_object['entities']['datetime'][0]['values'][0]['value']
+	return t[11:16]  # ugly, but parsing datetime strings is also fraught with shit. 
+
