aa761beff4dd4ab10c05348efffc600dc1ca2ae2
berkerboy <archy@AdministorsAir2.home>
2017-01-30 23:30:57 +0000

    a

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ def format_timepoint(message_text):
     # check if the correct values are returned by 
     if 'datetime' in msg_dict['entities']:
         log('found entities')
-        if 'values' in msg_dict['entitites']['datetime']:
+        if 'values' in msg_dict['entities']['datetime']:
             log('found datetime')
             return timestamp_to_simple_string(msg_dict)
     # value not found, return None
