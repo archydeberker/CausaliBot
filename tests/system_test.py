9fb9e6aed64487d0eeb508699816379c3692e491
archydeberker <archy.deberker@gmail.com>
2017-11-12 17:12:25 -0500

    Added a system test

--- tests/system_test.py
+++ tests/system_test.py
@@ -9,7 +9,8 @@ API_PORT = os.environ.get('API_PORT', '5000')
 base_url = "http://{}:{}/".format(API_HOST, API_PORT)
 base_path = os.path.abspath(os.path.dirname(__file__))
 
-def test_ping(self):
+
+def test_ping():
     res = requests.get(base_url + "ping")
     assert res.content == 'pong'
     assert res.status_code == 200
